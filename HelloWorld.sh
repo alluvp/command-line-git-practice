b5b5cfe3b10db86472e2ffbf6e3b1adbebff59a0
alluvp <avalliapp@gmail.com>
2025-10-16 16:44:56 -0700

    c2

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -3,7 +3,7 @@
 # and Command Line workshop. 
 
 NAME="Alagammai" # Change This Variable to Your Name
-
+MAJOR="Informatics"
 validate_name() {
     if [ "$NAME" = "HELLO WORLD" ]; then
         echo "You Haven't Changed the NAME Variable on Line 5. Please Change it!"
